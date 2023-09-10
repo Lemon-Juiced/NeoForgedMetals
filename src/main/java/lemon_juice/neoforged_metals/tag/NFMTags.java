512f7487e93d8de4a122aafe0a47e4890e7297b7
Lemon-Juiced <tbrotheawesome@gmail.com>
2023-09-10 18:47:22 -0400

    Added tags for all future metals

--- src/main/java/lemon_juice/neoforged_metals/tag/NFMTags.java
+++ src/main/java/lemon_juice/neoforged_metals/tag/NFMTags.java
@@ -34,6 +34,56 @@ public class NFMTags {
         public static final TagKey<Item> INGOTS_BRASS = forgeTag("ingots/brass");
         public static final TagKey<Item> INGOTS_ELECTRUM = forgeTag("ingots/electrum");
 
+        // Fantasy Metals
+        public static final TagKey<Item> INGOTS_ADAMANTINE = forgeTag("ingots/adamantine");
+        public static final TagKey<Item> INGOTS_AREDRITE = forgeTag("ingots/aredrite");
+        public static final TagKey<Item> INGOTS_ASTRAL_SILVER = forgeTag("ingots/astral_silver");
+        public static final TagKey<Item> INGOTS_ATLARUS = forgeTag("ingots/atlarus");
+        public static final TagKey<Item> INGOTS_CARMOT = forgeTag("ingots/carmot");
+        public static final TagKey<Item> INGOTS_DEEP_IRON = forgeTag("ingots/deep_iron");
+        public static final TagKey<Item> INGOTS_INFUSCOLIUM = forgeTag("ingots/infuscolium");
+        public static final TagKey<Item> INGOTS_MITHRIL = forgeTag("ingots/mithril");
+        public static final TagKey<Item> INGOTS_ORICHALCUM = forgeTag("ingots/orichalcum");
+        public static final TagKey<Item> INGOTS_OURECLASE = forgeTag("ingots/oureclase");
+        public static final TagKey<Item> INGOTS_PROMETHEUM = forgeTag("ingots/prometheum");
+        public static final TagKey<Item> INGOTS_RUBRACIUM = forgeTag("ingots/rubracium");
+
+        public static final TagKey<Item> INGOTS_BLACK_STEEL = forgeTag("ingots/black_steel");
+        public static final TagKey<Item> INGOTS_CENEGIL = forgeTag("ingots/cenegil");
+        public static final TagKey<Item> INGOTS_HADEROTH = forgeTag("ingots/haderoth");
+        public static final TagKey<Item> INGOTS_QUICKSILVER = forgeTag("ingots/quicksilver");
+        public static final TagKey<Item> INGOTS_TARTARITE = forgeTag("ingots/tartarite");
+
+        // Utility Metals
+        public static final TagKey<Item> GEMS_BITUMEN = forgeTag("gems/bitumen");
+        public static final TagKey<Item> GEMS_MAGNESIUM = forgeTag("gems/magnesium");
+        public static final TagKey<Item> GEMS_PHOSPHORITE = forgeTag("gems/phosphorite");
+        public static final TagKey<Item> GEMS_POTASH = forgeTag("gems/potash");
+        public static final TagKey<Item> GEMS_SALTPETER = forgeTag("gems/saltpeter");
+        public static final TagKey<Item> GEMS_SULFUR = forgeTag("gems/sulfur");
+
+        // Nether Metals
+        public static final TagKey<Item> INGOTS_ADLUORITE = forgeTag("ingots/adluorite");
+        public static final TagKey<Item> INGOTS_CERUCLASE = forgeTag("ingots/ceruclase");
+        public static final TagKey<Item> INGOTS_IGNATIUS = forgeTag("ingots/ignatius");
+        public static final TagKey<Item> INGOTS_KALENDRITE = forgeTag("ingots/kalendrite");
+        public static final TagKey<Item> INGOTS_LEMURITE = forgeTag("ingots/lemurite");
+        public static final TagKey<Item> INGOTS_MIDASIUM = forgeTag("ingots/midasium");
+        public static final TagKey<Item> INGOTS_SANGUINITE = forgeTag("ingots/sanguinite");
+        public static final TagKey<Item> INGOTS_SHADOW_IRON = forgeTag("ingots/shadow_iron");
+        public static final TagKey<Item> INGOTS_VULCANITE = forgeTag("ingots/vulcanite");
+        public static final TagKey<Item> INGOTS_VYROXERES = forgeTag("ingots/vyroxeres");
+
+        public static final TagKey<Item> INGOTS_AMORDRINE = forgeTag("ingots/amordrine");
+        public static final TagKey<Item> INGOTS_INOLASHITE = forgeTag("ingots/inolashite");
+        public static final TagKey<Item> INGOTS_SHADOW_STEEL = forgeTag("ingots/shadow_steel");
+
+        // End Metals
+        public static final TagKey<Item> INGOTS_EXIMITE = forgeTag("ingots/eximite");
+        public static final TagKey<Item> INGOTS_MEUTOITE = forgeTag("ingots/meutoite");
+
+        public static final TagKey<Item> INGOTS_DESICHALKOS = forgeTag("ingots/desichalkos");
+
         private static TagKey<Item> forgeTag(String name) {
             return ItemTags.create(new ResourceLocation("forge", name));
         }
